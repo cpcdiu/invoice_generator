333c078f60d25d0cd6c4a519df7782e7ec27cbe5
Md Mojahid <aammojahid@gmail.com>
2025-05-10 16:13:59 +0600

    Bug fixed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.mojahid.invoicegenerator"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 101
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
